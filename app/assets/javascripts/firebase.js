697b9f70da945141277d093cefd858e63798e1f7
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-03 17:52:57 -0700

    added firebase javascript file

---
+++ app/assets/javascripts/firebase.js
@@ -0,0 +1,12 @@
+var myFirebaseRef = new Firebase("https://vivid-inferno-3459.firebaseio.com/");
+
+// myFirebaseRef.createUser({
+//   email    : "bobtony@firebase.com",
+//   password : "correcthorsebatterystaple"
+// }, function(error, userData) {
+//   if (error) {
+//     console.log("Error creating user:", error);
+//   } else {
+//     console.log("Successfully created user account with uid:", userData.uid);
+//   }
+// });
