34a53e2a3ec0a3aae990e1c210f3714fadcaefbf
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-04 13:15:04 -0700

    added note for using user redirect versus user auth popup

--- app/assets/javascripts/firebase.js
+++ app/assets/javascripts/firebase.js
@@ -1,6 +1,7 @@
 $(function() {
 	var ref = new Firebase("https://vivid-inferno-3459.firebaseio.com");
 	$("#twitterLogin").click(function(){
+		// authWithOAuthRedirect
 		ref.authWithOAuthPopup("twitter", function(error, authData) {
   		if (error) {
     		console.log("Login Failed!", error);
