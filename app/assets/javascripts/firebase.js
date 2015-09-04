d1155cd301eadcabdfe10cff6b592ad92c6389ae
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-04 13:10:50 -0700

    contacted twitter login with firebase successfully, no view pages confirm yet

--- app/assets/javascripts/firebase.js
+++ app/assets/javascripts/firebase.js
@@ -1,10 +1,12 @@
-var myFirebaseRef = new Firebase("https://vivid-inferno-3459.firebaseio.com/");
-
-var ref = new Firebase("https://vivid-inferno-3459.firebaseio.com");
-ref.authWithOAuthPopup("twitter", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
+$(function() {
+	var ref = new Firebase("https://vivid-inferno-3459.firebaseio.com");
+	$("#twitterLogin").click(function(){
+		ref.authWithOAuthPopup("twitter", function(error, authData) {
+  		if (error) {
+    		console.log("Login Failed!", error);
+  		} else {
+    		console.log("Authenticated successfully with payload:", authData);
+  		}
+  	});
+	});
 });
