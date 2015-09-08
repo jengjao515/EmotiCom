8a25d7f5644ccccdf101f1563836d73488e61cd5
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-07 17:42:26 -0700

    fixed error where page had to refresh before user could click login with twitter button

--- app/assets/javascripts/firebase.js
+++ app/assets/javascripts/firebase.js
@@ -1,13 +1,15 @@
-$(function() {
-	var ref = new Firebase("https://vivid-inferno-3459.firebaseio.com");
-	$("#twitterLogin").click(function(){
-		// authWithOAuthRedirect
-		ref.authWithOAuthPopup("twitter", function(error, authData) {
-  		if (error) {
-    		console.log("Login Failed!", error);
-  		} else {
-    		console.log("Authenticated successfully with payload:", authData);
-  		}
-  	});
-	});
+$(document).on("page:change", function() {
+  $(function() {
+	 var ref = new Firebase("https://vivid-inferno-3459.firebaseio.com");
+	   $("#twitterLogin").click(function(){
+		  // authWithOAuthRedirect
+		  ref.authWithOAuthPopup("twitter", function(error, authData) {
+  		  if (error) {
+    		  console.log("Login Failed!", error);
+  		  } else {
+    		  console.log("Authenticated successfully with payload:", authData);
+  		  }
+  	 });
+	 });
+  });
 });
