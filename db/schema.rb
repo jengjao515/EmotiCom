7d0ffe0600b54ea83d0e906f88803d7c3f53dece
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-09 14:39:11 -0700

    upload to check for ouath error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909210814) do
+ActiveRecord::Schema.define(version: 20150909211920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
