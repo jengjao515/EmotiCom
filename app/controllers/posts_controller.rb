91531c276a1843a8ced9b779e5b1702dbcdcc3b5
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-11 13:56:58 -0700

    fixed firebase login error and rendered new post view page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
 	def new
+		@user = User.where(id: current_user.id).first
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'logout' => 'users#logout'
-  resources :users do 
-  	resources :posts
-  end
+  resources :users 
+  resources :posts
 end
