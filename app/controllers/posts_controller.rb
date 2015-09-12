87c6133654f7ef53f7b0d70fdb028f320184f5ca
Jennifer Jaochico <jenjao515@gmail.com>
2015-09-12 16:41:38 -0700

    fixed form error where user id wouldn't save with post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
 	def new
+		@post = Post.new
 		@user = User.where(id: current_user.id).first
 	end
+
+	def create
+		@post = Post.create(post_params)
+		@post.user_id = current_user.id
+	end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :password)
   end
 end
